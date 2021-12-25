b15a7f1e3f3206e20f22647d6287125d0652d3fe
gauthierlecompte <gauther.lecompte@student.uantwerpen.be>
2021-12-25 17:34:41 +0100

    comments

--- src/main.py
+++ src/main.py
@@ -17,6 +17,12 @@ def parse_csv(article):
     return articles
 
 def shingle(text, k):
+    """
+    Shingling splits the text up into tokens of size k, with no duplicates
+    :param text: The text we want to split up
+    :param k: the length of the tokens
+    :return: Set of all the shingles
+    """
     shingle_set = set()
 
     for i in range(len(text) - k+1):
@@ -25,6 +31,11 @@ def shingle(text, k):
     return shingle_set
 
 def unionize(articles):
+    """
+    Takes union of all the values in a dictionary
+    :param articles: Dictionary of articles we want to take the union of
+    :return: a list of all the values
+    """
     vocab = set()
 
     for article_id in articles:
@@ -33,6 +44,14 @@ def unionize(articles):
     return list(vocab)
 
 def one_hot_encoding(vocab, articles):
+    """
+    This function will loop over all the articles and items in the vocab list, and adds a 1 if the element exists in
+    the text of a article, otherwise it will add a 0. At the end of this function every article will have a corresponding
+    list with 0's or 1's which indicates whether the corresponding vocabulary item exists in the text or not.
+    :param vocab: The vocabulary which is a union of all the shingles of all the articles
+    :param articles: All the articles we want to check
+    :return: Dictionary where the key is the article id and the values are the hot encodings
+    """
     hot_encoded_articles = {}
     for article_id in articles:
         temp_hot_encoded = []
